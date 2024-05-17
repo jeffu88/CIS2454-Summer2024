60b429143e0566899ab6b9d37ce1abbbbfb7a0d3
Jeff Underwood <142617318+jeffu88@users.noreply.github.com>
2024-05-16 23:27:59 -0400

    Create index.php

---
+++ GitIntro/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo '<h2>Hello World!</h2>';
+        echo "</br> I'm Jeff Underwood";
+        echo "</br>Testing this thing out";
+        echo "</br>This is pretty neat :)";
+        ?>
+    </body>
+</html>
