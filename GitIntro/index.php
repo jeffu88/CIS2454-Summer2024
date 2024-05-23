9f1b3354bdf07f0ccb43612b6ad66310dc446840
Jeff Underwood <142617318+jeffu88@users.noreply.github.com>
2024-05-23 02:39:21 -0400

    Week 2

--- GitIntro/index.php
+++ GitIntro/index.php
@@ -6,15 +6,27 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Introducing Antonio</title>
     </head>
     <body style="background-color: lightblue;">
     <body>
+        <h1>Antonio's Intro</h1>
         <?php
-        echo '<h2>Hello World!</h2>';
-        echo "</br> I'm Jeff Underwood";
-        echo "</br>Testing this thing out";
-        echo "</br>This is pretty neat :)";
+        echo "<h2>A. C. World</h2>";
+        echo "<u></br>1 - Name, and major / reason for taking the class?</u>";
+        echo "<p>My name is Antonio Campbell, I have been enrolled for
+        more than a few semesters and hopefully I will have a degree in
+        Software Engineering after the coming fall semester and relax for a bit.</p>";
+        echo '<u></br>2 - Any previous programming experience that they have?</u>';
+        echo "<p>I have taken the following programing classes here at Occ: c++, Java, JavaScript, HTML and CSS.
+        I plan on working on this course at least 45 min to 2 hours a day 5 out of the 7 days in a week.
+        I am taking this class to get a more complete understanding of web development.
+        Knowing how to use Apache and php would allow me to make fully dynamic website
+        complete with a database if needed.</p>";
+        echo "<u></br>3 - Do they have any hobbies?</u>";
+        echo "<p>My hobbies include renovating my home, and playing Indy video games.</p>";
+        echo "<u></br>4 - Anything else they'd like the class to know about them?</u>";
+        echo "<p>I firmly believe Epstein didn’t ---- himself.</p>";
         ?>
     </body>
 </html>
